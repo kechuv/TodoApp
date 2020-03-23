5c58e955cbc9b5d19ac5a960388d3f863fc48e1e
Kheops Valencia <kheopsvalencia@gmail.com>
2020-03-23 14:14:18 -0600

    deploy

---
+++ precache-manifest.3f7bbb6899d13614b0db742692b96df7.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3cd04362911b2aef5dc",
+    "url": "/TodoApp/css/app.88324a9d.css"
+  },
+  {
+    "revision": "ef93a70bdbf655a334e2",
+    "url": "/TodoApp/css/chunk-0f2dbc40.1d97ac82.css"
+  },
+  {
+    "revision": "a6a4348e72d948d0f27e",
+    "url": "/TodoApp/css/chunk-3f947e1e.d694473a.css"
+  },
+  {
+    "revision": "85f8a2a66c12466fbbd5",
+    "url": "/TodoApp/css/chunk-f14c515e.09e54930.css"
+  },
+  {
+    "revision": "03546bc9b6577c7a2c54ea2f3f512873",
+    "url": "/TodoApp/favicon.png"
+  },
+  {
+    "revision": "0c056df65bb1719974106a9c5a290b66",
+    "url": "/TodoApp/fonts/Geomanist-Black.0c056df6.otf"
+  },
+  {
+    "revision": "748e4c83bd1adccc610a6d00af37fa0b",
+    "url": "/TodoApp/fonts/Geomanist-Bold.748e4c83.otf"
+  },
+  {
+    "revision": "7aa547699c1e2d98b20d85116cb9e57e",
+    "url": "/TodoApp/fonts/Geomanist-Light.7aa54769.otf"
+  },
+  {
+    "revision": "4ba648bdf89bde9972fd34d3d81f9835",
+    "url": "/TodoApp/index.html"
+  },
+  {
+    "revision": "f3cd04362911b2aef5dc",
+    "url": "/TodoApp/js/app.a4391007.js"
+  },
+  {
+    "revision": "ef93a70bdbf655a334e2",
+    "url": "/TodoApp/js/chunk-0f2dbc40.577ebf37.js"
+  },
+  {
+    "revision": "a6a4348e72d948d0f27e",
+    "url": "/TodoApp/js/chunk-3f947e1e.0970d502.js"
+  },
+  {
+    "revision": "85f8a2a66c12466fbbd5",
+    "url": "/TodoApp/js/chunk-f14c515e.94967518.js"
+  },
+  {
+    "revision": "628e1efaef8aa9749eae",
+    "url": "/TodoApp/js/chunk-vendors.16762915.js"
+  },
+  {
+    "revision": "67df43d64dd1a6f014decb576bf27a7e",
+    "url": "/TodoApp/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/TodoApp/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,27 @@
+importScripts("/TodoApp/precache-manifest.3f7bbb6899d13614b0db742692b96df7.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/* eslint-disable */
+// This is the code piece that GenerateSW mode can't provide for us.
+// This code listens for the user's confirmation to update the app.
+self.addEventListener('message', (e) => {
+  if (!e.data) {
+    return;
+  }
+
+  switch (e.data) {
+    case 'skipWaiting':
+      self.skipWaiting();
+      break;
+    default:
+      // NOOP
+      break;
+  }
+});
+
+workbox.core.clientsClaim(); // Vue CLI 4 and Workbox v4, else
+// workbox.clientsClaim(); // Vue CLI 3 and Workbox v3.
+
+// The precaching code provided by Workbox.
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+// workbox.precaching.suppressWarnings(); // Only used with Vue CLI 3 and Workbox v3.
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
